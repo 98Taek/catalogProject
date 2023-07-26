c66e369ddceda02751a0e58febde47be9fe48b9f
Jeong Taek <imjt9345@naver.com>
2023-07-26 13:41:22 +0900

    기본 포스트 목록 페이지, 상세 페이지 추가

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from blog.models import Post
 
 
 def post_list(request):
-    pass
+    posts = Post.objects.filter(status=Post.Status.PUBLISHED).all()
+    return render(request, 'blog/post/post_list.html', {'posts': posts})
 
 
-def post_detail(request, id):
-    pass
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    return render(request, 'blog/post/post_detail.html', {'post': post})
