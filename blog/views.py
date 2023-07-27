ce5421dae68df7f329386d70f84614edbce2ee02
Jeong Taek <imjt9345@naver.com>
2023-07-27 10:45:08 +0900

    뷰 함수, 템플릿 추가

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
+from django.core.mail import send_mail
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render, get_object_or_404
 
+from blog.forms import EmailPostForm
 from blog.models import Post
 
 
@@ -22,3 +24,21 @@ def post_list(request):
 def post_detail(request, post_id):
     post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
     return render(request, 'blog/post/post_detail.html', {'post': post})
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{data['name']} 님이 {post.title}을(를) 추천합니다."
+            message = f"{post.title} 을(를) {post_url}에서 읽어보세요.\n\n" \
+                      f"{data['name']}의 의견: {data['message']}"
+            send_mail(subject, message, '98taek@gamil.com', [data['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/post_share.html', {'post': post, 'form': form, 'sent': sent})
