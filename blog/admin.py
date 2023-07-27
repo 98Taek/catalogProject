dfec582f284a19e7399632ac30f69b3d1a0584e4
Jeong Taek <imjt9345@naver.com>
2023-07-27 11:42:36 +0900

    comment admin 사이트 등록

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 
-from blog.models import Post
+from blog.models import Post, Comment
 
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ['title', 'author', 'publish', 'updated', 'status']
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'body', 'created', 'active']
+    list_filter = ['active', 'created', 'updated']
+    search_fields = ['name', 'email', 'body']
