2538743406ffd77eb1f65c9a9c734306a536c373
Jeong Taek <imjt9345@naver.com>
2023-08-10 13:45:45 +0900

    Product 모델 추가, 마이그레이션

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+from blog.models import Post
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    posts = models.ManyToManyField(Post, related_name='products')
+    description = models.TextField()
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created']
+        indexes = [
+            models.Index(fields=['-created']),
+        ]
+
+    def __str__(self):
+        return self.name
