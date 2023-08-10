5668a86c05fd6b4189757a23d3a7a8213b18e389
Jeong Taek <imjt9345@naver.com>
2023-08-10 14:22:15 +0900

    Product url, 뷰 함수 추가

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from shop.models import Product
+
+
+def product_list(request):
+    products = Product.objects.filter(available=True)
+    return render(request, 'shop/product/list.html', {'products': products})
+
+
+def product_detail(request, product_id):
+    product = get_object_or_404(Product, id=product_id, available=True)
+    return render(request, 'shop/product/detail.html', {'product': product})
