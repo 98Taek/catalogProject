1610d260ed8aa718da92cd8adb7782dfede25e02
Jeong Taek <imjt9345@naver.com>
2023-08-10 14:36:53 +0900

    admin 페이지 수정

--- shop/admin.py
+++ shop/admin.py
@@ -5,6 +5,7 @@ from shop.models import Product
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['name', 'price', 'created', 'updated', 'available']
+    list_display = ['id', 'name', 'price', 'created', 'updated', 'available']
+    list_editable = ['name', 'price', 'available']
     list_filter = ['created', 'available']
     raw_id_fields = ['posts']
