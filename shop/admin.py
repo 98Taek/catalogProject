6fb1fbd0e0a4607fecae76b1e40031ca229dff30
Jeong Taek <imjt9345@naver.com>
2023-08-10 14:20:03 +0900

    Product admin 추가

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from shop.models import Product
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'created', 'updated', 'available']
+    list_filter = ['created', 'available']
+    raw_id_fields = ['posts']
