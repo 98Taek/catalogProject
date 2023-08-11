a7ce897f8e765488321fc6a22f50921cba8afae5
Jeong Taek <imjt9345@naver.com>
2023-08-11 13:47:04 +0900

    order admin 추가

--- shop/admin.py
+++ shop/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from shop.models import Product
+from shop.models import Product, Order, OrderItem
 
 
 @admin.register(Product)
@@ -9,3 +9,15 @@ class ProductAdmin(admin.ModelAdmin):
     list_editable = ['name', 'price', 'available']
     list_filter = ['created', 'available']
     raw_id_fields = ['posts']
+
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    raw_id_fields = ['product']
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'paid', 'get_total_cost', 'created', 'updated']
+    list_filter = ['paid', 'created', 'updated']
+    inlines = [OrderItemInline]
