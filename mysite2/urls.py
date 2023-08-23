49c114e6bf00772090f9ed39c88b82ffce2c923a
Jeong Taek <imjt9345@naver.com>
2023-08-23 10:54:17 +0900

    언어 선택 추가, url 국제화

--- mysite2/urls.py
+++ mysite2/urls.py
@@ -15,14 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', includes('blog.urls'))
 """
 from django.conf import settings
+from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [
+urlpatterns = i18n_patterns(
     path('admin/', admin.site.urls),
     path('rosetta/', include('rosetta.urls')),
 
-    path('blog/', include('blog.urls')),
     path('shop/', include('shop.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('', include('blog.urls', namespace='blog')),
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
