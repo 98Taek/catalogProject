c2775f5462b7d738710aae20648c769852e76ae1
Jeong Taek <imjt9345@naver.com>
2023-08-23 23:36:33 +0900

    회원가입 클래스 뷰 추가

--- account/urls.py
+++ account/urls.py
@@ -3,4 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
+    path('regsiter/', RegisterView.as_view(), name='register'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-# Create your views here.
+
+class CustomLoginView(LoginView):
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('dashboard')
+        return super().dispatch(request, *args, **kwargs)
+
+
+class RegisterView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/register.html'
