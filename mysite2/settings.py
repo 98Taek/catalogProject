472a1eae39f5d65747a135a34da99caa04fb1339
Jeong Taek <imjt9345@naver.com>
2023-08-23 09:42:44 +0900

    rosetta 설치, LANGUAGE 설정

--- mysite2/settings.py
+++ mysite2/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from django.utils.translation import gettext_lazy as _
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -41,11 +42,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.humanize',
     'taggit',
+    'rosetta',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -111,7 +114,12 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en'
+
+LANGUAGES = [
+    ('en', _('English')),
+    ('ko', _('Korean')),
+]
 
 TIME_ZONE = 'UTC'
 
@@ -155,4 +163,8 @@ CACHES = {
     }
 }
 
-CELERY_TASK_ALWAYS_EAGER = DEBUG
+CELERY_TASK_ALWAYS_EAGER = True
+
+LOCALE_PATHS = [
+    BASE_DIR / 'locale',
+]
